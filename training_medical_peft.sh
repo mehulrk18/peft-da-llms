35580166a80056387d1b3bd7b5f0ab5518ae6c08
mehul <mehul.kumawat@tum.de>
2024-11-26 21:36:09 +0100

    .gitignore added

--- training_medical_peft.sh
+++ training_medical_peft.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#echo "Starting training Medical Pubmed loha" && \
+#echo "Starting training Medical Pubmed Lokr" && \
 #time python training_one_v_one.py \
 #  --provider "hf" \
-#  --peft "loha" \
+#  --peft "lokr" \
 #  --domain "medical" \
 #  --dataset "pubmed" \
 #  --train_epochs 5 \
@@ -15,15 +15,32 @@
 #  --batch_size 4 \
 #  --torch_dtype "bf16" \
 #  --return_overflowing_tokens "True"
-#echo "Finished training Medical Pubmed loha"
+#echo "Finished training Medical Pubmed Lokr"
 
+#echo "Starting training Medical Pubmed AdaLora" && \
+#time python training_one_v_one.py \
+#  --provider "hf" \
+#  --peft "adalora" \
+#  --domain "medical" \
+#  --dataset "pubmed" \
+#  --train_epochs 5 \
+#  --tokenization_with_attention "True" \
+#  --max_seq_len 4096 \
+#  --training_samples 1000 \
+#  --eval_samples 250 \
+#  --test_samples 1 \
+#  --sorted_dataset "True" \
+#  --batch_size 4 \
+#  --torch_dtype "bf16" \
+#  --return_overflowing_tokens "True"
+#echo "Finished training Medical Pubmed adalora"
 
-echo "Starting training Medical Scilay loha" && \
+echo "Starting training Medical Pubmed llamaadapter" && \
 time python training_one_v_one.py \
   --provider "hf" \
-  --peft "loha" \
+  --peft "llamaadapter" \
   --domain "medical" \
-  --dataset "scilay" \
+  --dataset "pubmed" \
   --train_epochs 5 \
   --tokenization_with_attention "True" \
   --max_seq_len 4096 \
@@ -34,40 +51,80 @@ time python training_one_v_one.py \
   --batch_size 4 \
   --torch_dtype "bf16" \
   --return_overflowing_tokens "True"
-echo "Finished training Medical Scilay loha"
+echo "Finished training Medical Pubmed llamaadapter"
 
-echo "Starting training Medical MSLR loha" && \
-time python training_one_v_one.py \
-  --provider "hf" \
-  --peft "loha" \
-  --domain "medical" \
-  --dataset "mslr" \
-  --train_epochs 5 \
-  --tokenization_with_attention "True" \
-  --max_seq_len 4096 \
-  --training_samples 1000 \
-  --eval_samples 250 \
-  --test_samples 1 \
-  --sorted_dataset "True" \
-  --batch_size 4 \
-  --torch_dtype "bf16" \
-  --return_overflowing_tokens "True"
-echo "Finished training Medical MSLR loha"
+#echo "Starting training Medical Pubmed oft" && \
+#time python training_one_v_one.py \
+#  --provider "hf" \
+#  --peft "oft" \
+#  --domain "medical" \
+#  --dataset "pubmed" \
+#  --train_epochs 5 \
+#  --tokenization_with_attention "True" \
+#  --max_seq_len 4096 \
+#  --training_samples 1000 \
+#  --eval_samples 250 \
+#  --test_samples 1 \
+#  --sorted_dataset "True" \
+#  --batch_size 4 \
+#  --torch_dtype "bf16" \
+#  --return_overflowing_tokens "True"
+#echo "Finished training Medical Pubmed oft"
 
-echo "Starting training Medical Cord19 loha" && \
-time python training_one_v_one.py \
-  --provider "hf" \
-  --peft "loha" \
-  --domain "medical" \
-  --dataset "cord19" \
-  --train_epochs 5 \
-  --tokenization_with_attention "True" \
-  --max_seq_len 4096 \
-  --training_samples 1000 \
-  --eval_samples 250 \
-  --test_samples 1 \
-  --sorted_dataset "True" \
-  --batch_size 4 \
-  --torch_dtype "bf16" \
-  --return_overflowing_tokens "True"
-echo "Finished training Medical Cord19 loha"
+echo "!!Trained Medical's Pubmed Datasets on LoRa, Loha, IA3, Lokr, AdaLora, LlamaAdapter, and OFT!!"
+
+#echo "Starting training Medical Scilay ia3" && \
+#time python training_one_v_one.py \
+#  --provider "hf" \
+#  --peft "ia3" \
+#  --domain "medical" \
+#  --dataset "scilay" \
+#  --train_epochs 5 \
+#  --tokenization_with_attention "True" \
+#  --max_seq_len 4096 \
+#  --training_samples 1000 \
+#  --eval_samples 250 \
+#  --test_samples 1 \
+#  --sorted_dataset "True" \
+#  --batch_size 4 \
+#  --torch_dtype "bf16" \
+#  --return_overflowing_tokens "True"
+#echo "Finished training Medical Scilay ia3"
+#
+#echo "Starting training Medical MSLR ia3" && \
+#time python training_one_v_one.py \
+#  --provider "hf" \
+#  --peft "ia3" \
+#  --domain "medical" \
+#  --dataset "mslr" \
+#  --train_epochs 5 \
+#  --tokenization_with_attention "True" \
+#  --max_seq_len 4096 \
+#  --training_samples 1000 \
+#  --eval_samples 250 \
+#  --test_samples 1 \
+#  --sorted_dataset "True" \
+#  --batch_size 4 \
+#  --torch_dtype "bf16" \
+#  --return_overflowing_tokens "True"
+#echo "Finished training Medical MSLR ia3"
+#
+#echo "Starting training Medical Cord19 ia3" && \
+#time python training_one_v_one.py \
+#  --provider "hf" \
+#  --peft "ia3" \
+#  --domain "medical" \
+#  --dataset "cord19" \
+#  --train_epochs 5 \
+#  --tokenization_with_attention "True" \
+#  --max_seq_len 4096 \
+#  --training_samples 1000 \
+#  --eval_samples 250 \
+#  --test_samples 1 \
+#  --sorted_dataset "True" \
+#  --batch_size 4 \
+#  --torch_dtype "bf16" \
+#  --return_overflowing_tokens "True"
+#echo "Finished training Medical Cord19 ia3"
+#
+#echo "!!Trained Medical Datasets on LoRa, Loha and IA3!!"
--- training_scientific_peft.sh
+++ training_scientific_peft.sh
@@ -1,8 +1,44 @@
 #!/bin/bash
-echo "Starting training Scientific Arxiv ia3" && \
+#echo "Starting training Scientific Arxiv Lokr" && \
+#time python training_one_v_one.py \
+#  --provider "hf" \
+#  --peft "lokr" \
+#  --domain "scientific" \
+#  --dataset "arxiv" \
+#  --train_epochs 5 \
+#  --tokenization_with_attention "True" \
+#  --max_seq_len 4096 \
+#  --training_samples 1000 \
+#  --eval_samples 250 \
+#  --test_samples 1 \
+#  --sorted_dataset "True" \
+#  --batch_size 4 \
+#  --torch_dtype "bf16" \
+#  --return_overflowing_tokens "True"
+#echo "Finished training Scientific Arxiv Lokr"
+
+#echo "Starting training Scientific Arxiv AdaLora" && \
+#time python training_one_v_one.py \
+#  --provider "hf" \
+#  --peft "adalora" \
+#  --domain "scientific" \
+#  --dataset "arxiv" \
+#  --train_epochs 5 \
+#  --tokenization_with_attention "True" \
+#  --max_seq_len 4096 \
+#  --training_samples 1000 \
+#  --eval_samples 250 \
+#  --test_samples 1 \
+#  --sorted_dataset "True" \
+#  --batch_size 4 \
+#  --torch_dtype "bf16" \
+#  --return_overflowing_tokens "True"
+#echo "Finished training Scientific Arxiv adalora"
+
+echo "Starting training Scientific Arxiv llamaadapter" && \
 time python training_one_v_one.py \
   --provider "hf" \
-  --peft "ia3" \
+  --peft "llamaadapter" \
   --domain "scientific" \
   --dataset "arxiv" \
   --train_epochs 5 \
@@ -15,43 +51,64 @@ time python training_one_v_one.py \
   --batch_size 4 \
   --torch_dtype "bf16" \
   --return_overflowing_tokens "True"
-echo "Finished training Scientific Arxiv ia3"
+echo "Finished training Scientific Arxiv llamaadapter"
+
+#echo "Starting training Scientific Arxiv oft" && \
+#time python training_one_v_one.py \
+#  --provider "hf" \
+#  --peft "oft" \
+#  --domain "scientific" \
+#  --dataset "arxiv" \
+#  --train_epochs 5 \
+#  --tokenization_with_attention "True" \
+#  --max_seq_len 4096 \
+#  --training_samples 1000 \
+#  --eval_samples 250 \
+#  --test_samples 1 \
+#  --sorted_dataset "True" \
+#  --batch_size 4 \
+#  --torch_dtype "bf16" \
+#  --return_overflowing_tokens "True"
+#echo "Finished training Scientific Arxiv oft"
+
+echo "!!Trained Scientific's Arxiv Datasets on LoRa, Loha, IA3, Lokr, AdaLora, LlamaAdapter, and OFT!!"
 
 
-echo "Starting training Scientific Elsevier ia3" && \
-time python training_one_v_one.py \
-  --provider "hf" \
-  --peft "ia3" \
-  --domain "scientific" \
-  --dataset "elsevier" \
-  --train_epochs 5 \
-  --tokenization_with_attention "True" \
-  --max_seq_len 4096 \
-  --training_samples 1000 \
-  --eval_samples 250 \
-  --test_samples 1 \
-  --sorted_dataset "True" \
-  --batch_size 4 \
-  --torch_dtype "bf16" \
-  --return_overflowing_tokens "True"
-echo "Finished training Scientific Elsevier ia3"
 
-echo "Starting training Scientific Scitldr ia3" && \
-time python training_one_v_one.py \
-  --provider "hf" \
-  --peft "ia3" \
-  --domain "scientific" \
-  --dataset "scitldr" \
-  --train_epochs 5 \
-  --tokenization_with_attention "True" \
-  --max_seq_len 4096 \
-  --training_samples 1000 \
-  --eval_samples 250 \
-  --test_samples 1 \
-  --sorted_dataset "True" \
-  --batch_size 4 \
-  --torch_dtype "bf16" \
-  --return_overflowing_tokens "True"
-echo "Finished training Scientific Scitldr ia3"
-
-echo "!!Trained Scientific Datasets on LoRa, Loha and IA3!!"
+#echo "Starting training Scientific Elsevier ia3" && \
+#time python training_one_v_one.py \
+#  --provider "hf" \
+#  --peft "ia3" \
+#  --domain "scientific" \
+#  --dataset "elsevier" \
+#  --train_epochs 5 \
+#  --tokenization_with_attention "True" \
+#  --max_seq_len 4096 \
+#  --training_samples 1000 \
+#  --eval_samples 250 \
+#  --test_samples 1 \
+#  --sorted_dataset "True" \
+#  --batch_size 4 \
+#  --torch_dtype "bf16" \
+#  --return_overflowing_tokens "True"
+#echo "Finished training Scientific Elsevier ia3"
+#
+#echo "Starting training Scientific Scitldr ia3" && \
+#time python training_one_v_one.py \
+#  --provider "hf" \
+#  --peft "ia3" \
+#  --domain "scientific" \
+#  --dataset "scitldr" \
+#  --train_epochs 5 \
+#  --tokenization_with_attention "True" \
+#  --max_seq_len 4096 \
+#  --training_samples 1000 \
+#  --eval_samples 250 \
+#  --test_samples 1 \
+#  --sorted_dataset "True" \
+#  --batch_size 4 \
+#  --torch_dtype "bf16" \
+#  --return_overflowing_tokens "True"
+#echo "Finished training Scientific Scitldr ia3"
+#
+#echo "!!Trained Scientific Datasets on LoRa, Loha and IA3!!"
