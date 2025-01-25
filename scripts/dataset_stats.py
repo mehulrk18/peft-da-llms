5cb1efac64f756ef440263001d91adee70b45827
mehul <mehul.kumawat@tum.de>
2025-01-25 06:02:00 +0100

    calculating avg len of articles

--- scripts/dataset_stats.py
+++ scripts/dataset_stats.py
@@ -16,8 +16,10 @@ def calculate_stats(llama, samples, dest_file: str):
                     data = pd.read_excel(p.local_path)
                 else:
                     data = pd.read_csv(p.local_path)
-                test_df["content_tokens"] = [len(llama.tokenizer.tokenize(x)) for x in data["content"].tolist()]
-                test_df["summary_tokens"] = [len(llama.tokenizer.tokenize(x)) for x in data["summary"].tolist()]
+                # test_df["content_tokens"] = [len(llama.tokenizer.tokenize(x)) for x in data["content"].tolist()]
+                # test_df["summary_tokens"] = [len(llama.tokenizer.tokenize(x)) for x in data["summary"].tolist()]
+                test_df["content_len"] = [len(x) for x in data["content"].tolist()]
+                test_df["summary_len"] = [len(x) for x in data["summary"].tolist()]
             else:
                 data = SumDataLoader(
                     domain=domain.name.lower(),
@@ -28,28 +30,40 @@ def calculate_stats(llama, samples, dest_file: str):
                     sort_dataset_on_article_len=True
                 )
 
-                train_df["content_tokens"] = [len(llama.tokenizer.tokenize(x)) for x in data.train_set["content"]]
-                train_df["summary_tokens"] = [len(llama.tokenizer.tokenize(x)) for x in data.train_set["summary"]]
+                # train_df["content_tokens"] = [len(llama.tokenizer.tokenize(x)) for x in data.train_set["content"]]
+                # train_df["summary_tokens"] = [len(llama.tokenizer.tokenize(x)) for x in data.train_set["summary"]]
+                train_df["content_len"] = [len(x) for x in data.train_set["content"]]
+                train_df["summary_len"] = [len(x) for x in data.train_set["summary"]]
 
-                val_df["content_tokens"] = [len(llama.tokenizer.tokenize(x)) for x in data.validation_set["content"]]
-                val_df["summary_tokens"] = [len(llama.tokenizer.tokenize(x)) for x in data.validation_set["summary"]]
+                # val_df["content_tokens"] = [len(llama.tokenizer.tokenize(x)) for x in data.validation_set["content"]]
+                # val_df["summary_tokens"] = [len(llama.tokenizer.tokenize(x)) for x in data.validation_set["summary"]]
+                val_df["content_len"] = [len(x) for x in data.validation_set["content"]]
+                val_df["summary_len"] = [len(x) for x in data.validation_set["summary"]]
 
-                test_df["content_tokens"] = [len(llama.tokenizer.tokenize(x)) for x in data.test_set["content"]]
-                test_df["summary_tokens"] = [len(llama.tokenizer.tokenize(x)) for x in data.test_set["summary"]]
+                # test_df["content_tokens"] = [len(llama.tokenizer.tokenize(x)) for x in data.test_set["content"]]
+                # test_df["summary_tokens"] = [len(llama.tokenizer.tokenize(x)) for x in data.test_set["summary"]]
+                test_df["content_len"] = [len(x) for x in data.test_set["content"]]
+                test_df["summary_len"] = [len(x) for x in data.test_set["summary"]]
 
             print(f"**** Domain: {domain} ---  Dataset: {_dataset} ***")
             obj = {
                 "domain": domain,
                 "dataset": _dataset,
                 "train_samples": len(train_df),
-                "train_average_content_tokens": train_df["content_tokens"].mean() if len(train_df) > 0 else 0,
-                "train_average_summary_tokens": train_df["summary_tokens"].mean() if len(train_df) > 0 else 0,
+                # "train_average_content_tokens": train_df["content_tokens"].mean() if len(train_df) > 0 else 0,
+                # "train_average_summary_tokens": train_df["summary_tokens"].mean() if len(train_df) > 0 else 0,
+                "train_average_content_len": train_df["content_len"].mean() if len(train_df) > 0 else 0,
+                "train_average_summary_len": train_df["summary_len"].mean() if len(train_df) > 0 else 0,
                 "validation_samples": len(val_df),
-                "validation_average_content_tokens": val_df["content_tokens"].mean() if len(val_df) > 0 else 0,
-                "validation_average_summary_tokens": val_df["summary_tokens"].mean() if len(val_df) > 0 else 0,
+                # "validation_average_content_tokens": val_df["content_tokens"].mean() if len(val_df) > 0 else 0,
+                # "validation_average_summary_tokens": val_df["summary_tokens"].mean() if len(val_df) > 0 else 0,
+                "validation_average_content_len": val_df["content_len"].mean() if len(val_df) > 0 else 0,
+                "validation_average_summary_len": val_df["summary_len"].mean() if len(val_df) > 0 else 0,
                 "test_samples": len(test_df),
-                "test_average_content_tokens": test_df["content_tokens"].mean() if len(test_df) > 0 else 0,
-                "test_average_summary_tokens": test_df["summary_tokens"].mean() if len(test_df) > 0 else 0
+                # "test_average_content_tokens": test_df["content_tokens"].mean() if len(test_df) > 0 else 0,
+                # "test_average_summary_tokens": test_df["summary_tokens"].mean() if len(test_df) > 0 else 0
+                "test_average_content_len": test_df["content_len"].mean() if len(test_df) > 0 else 0,
+                "test_average_summary_len": test_df["summary_len"].mean() if len(test_df) > 0 else 0
             }
             data_statss.append(obj)
             # print("\n\nAvg Tokens Articles in Training Set:", train_df["content_tokens"].mean())
@@ -76,12 +90,14 @@ if __name__ == "__main__":
         "validation": 5000,
         "test": 5000
     }
-    llama = LLaMAModelClass(
-        instruct_mode=True,
-        mlm=True
-    )
-    llama.model = None
-    del llama.model
+    # llama = LLaMAModelClass(
+    #     instruct_mode=True,
+    #     mlm=True
+    # )
+    # llama.model = None
+    # del llama.model
 
-    calculate_stats(llama, used_samples, "results/dataset_used_samples_stats.xlsx")
-    calculate_stats(llama, max_samples, "results/dataset_max_samples_stats.xlsx")
+    llama = None
+
+    calculate_stats(llama, used_samples, "results/dataset_used_samples_stats_len.xlsx")
+    calculate_stats(llama, max_samples, "results/dataset_max_samples_stats_len.xlsx")
